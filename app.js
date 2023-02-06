d21bfd24d242fc98020e3750839d80da85c304d5
CGLIFE7 <75388473+CGLIFE7@users.noreply.github.com>
2023-02-06 23:44:49 +1300

    maybe

--- app.js
+++ app.js
@@ -6,9 +6,10 @@ const path = require('path');
 
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-})
+app.get('*', function (req, res) {
+  const index = path.join(__dirname, 'build', 'index.html');
+  res.sendFile(index);
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
