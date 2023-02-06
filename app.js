44582ee4026670a757e32c22704bcc31f995dc31
CGLIFE7 <75388473+CGLIFE7@users.noreply.github.com>
2023-02-06 21:56:32 +1300

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
+const path = require('path');
+
 
 app.use(express.static(path.join(__dirname, 'build')));
 
